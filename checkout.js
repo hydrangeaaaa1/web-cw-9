db1388f5aa59bf63943b08f17325ba6f1eb1362f
haya <hydrangeaaaa1@users.noreply.github.com>
2021-01-09 01:18:52 +0300

    done with all bonus

--- checkout.js
+++ checkout.js
@@ -1,47 +1,3 @@
-let products = [
-    {name: 'Creme Brulee Cookie', price: 0.5 },
-    {name: 'White Choco Orea Cookie', price: 0.75},
-    {name: 'Mixed Dates Cookie', price:0.3 }
-];
-
-
-
-
-let cart =[];
-
-function clearCart(){
- cart =[];
-}
-
-
-
-function addproduct(index){
-let product = products[index];
-let quantity = document.getElementById(index).value;
-product.quantity = quantity;
-cart.push(product);
-console.log(cart);
-localStorage.setItem(product.name, product.quantity, product.price);
-// doShowAll();
-let sum = 0;
-
-document.getElementById("cart").innerHTML = "";
-
-for (let index = 0; index < cart.length; index++) {
-    const element = cart[index];
-    let price = element.price * element.quantity;
-    sum = sum + price;
-
-    let list = document.getElementById("cart");
-    list.innerHTML+= `<li> ${element.quantity}  ${element.name}  ${element.price} KD<li>`;
-}
-
-
-document.getElementById("total").innerText = `${sum}KD`;
-}
-
-
-
 function doShowAll() {
     if (CheckBrowser()) {
         var key = "";
@@ -100,22 +56,30 @@ function doShowAll() {
 }
 
 
+function SaveItem() {
 
-// function ModifyItem() {
-//     var name1 = document.forms.ShoppingList.name.value;
-//     var data1 = document.forms.ShoppingList.data.value;
-//     //check if name1 is already exists
+    var name = document.forms.ShoppingList.name.value;
+    var data = document.forms.ShoppingList.data.value;
+    localStorage.setItem(name, data);
+    doShowAll();
 
-// //Check if key exists.
-//             if (localStorage.getItem(name1) !=null)
-//             {
-//               //update
-//               localStorage.setItem(name1,data1);
-//               document.forms.ShoppingList.data.value = localStorage.getItem(name1);
-//             }
+}
 
-//     doShowAll();
-// }
+function ModifyItem() {
+    var name1 = document.forms.ShoppingList.name.value;
+    var data1 = document.forms.ShoppingList.data.value;
+    //check if name1 is already exists
+
+//Check if key exists.
+            if (localStorage.getItem(name1) !=null)
+            {
+              //update
+              localStorage.setItem(name1,data1);
+              document.forms.ShoppingList.data.value = localStorage.getItem(name1);
+            }
+
+    doShowAll();
+}
 function RemoveItem()
 {
 var name=document.forms.ShoppingList.name.value;
